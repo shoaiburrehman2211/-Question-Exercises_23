533b5ead680d52e43304127596a500da0fc92bd1
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 04:25:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+Write a series of conditional tests. Print a statement describing each test and your prediction
+for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+var myCar = "Alto";
+//console.log(myCar == "Alto");
+// single (=) is uesd for assigning and (==) is used for compartision
+// 10 statements true and false
+// 01.
+var myBike = "Honda";
+//console.log(myBike == "honda");
+//02.
+var myArray = [13, 3, 9];
+//console.log(myArray.length == 3);
+//03.
+var stdName = "Junaid";
+//console.log(stdName == "Ali");
+//console.log(stdName == "ali");
+//04.
+var mySchool = "Government";
+//console.log(mySchool == "Government");
+//console.log(mySchool === "Government");
+//05.
+var lang = "Urdu";
+//console.log(lang == "UrdU");
+//console.log(lang === "Urdu");
+//06.
+var myClass = "Nine";
+//console.log(myClass == "Nine");
+//console.log(myClass === "Nine");
+//07.
+var myFriends = "Ayaz";
+//console.log(myFriends == "AyaZ");
+//console.log(myFriends == "Ayaz");
+//08.
+var myHouse = "Armish";
+//console.log(myHouse == "Armish");
+//console.log(myHouse === "Armish");
+//09.
+var Fruit = "Banana";
+//console.log(Fruit == "BananA");
+//console.log(Fruit == "Banana");
+//10.
+var Lang = "English";
+console.log(Lang == "EnglisH");
+console.log(Lang === "English");
---
+++ main.ts
@@ -0,0 +1,75 @@
+/*
+Write a series of conditional tests. Print a statement describing each test and your prediction 
+for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+
+
+let myCar = "Alto";
+//console.log(myCar == "Alto");
+
+// single (=) is uesd for assigning and (==) is used for compartision
+
+// 10 statements true and false
+
+// 01.
+
+let myBike = "Honda";
+//console.log(myBike == "honda");
+
+//02.
+
+let myArray = [13, 3, 9];
+//console.log(myArray.length == 3);
+
+//03.
+
+let stdName = "Junaid";
+//console.log(stdName == "Ali");
+//console.log(stdName == "ali");
+
+//04.
+
+let mySchool = "Government";
+//console.log(mySchool == "Government");
+//console.log(mySchool === "Government");
+
+//05.
+
+let lang = "Urdu";
+//console.log(lang == "UrdU");
+//console.log(lang === "Urdu");
+
+//06.
+
+let myClass = "Nine";
+//console.log(myClass == "Nine");
+//console.log(myClass === "Nine");
+
+//07.
+
+let myFriends = "Ayaz";
+//console.log(myFriends == "AyaZ");
+//console.log(myFriends == "Ayaz");
+
+//08.
+
+let myHouse = "Armish";
+//console.log(myHouse == "Armish");
+//console.log(myHouse === "Armish");
+
+//09.
+
+let Fruit = "Banana";
+//console.log(Fruit == "BananA");
+//console.log(Fruit == "Banana");
+
+//10.
+
+let Lang = "English"
+console.log(Lang == "EnglisH");
+console.log(Lang === "English");
